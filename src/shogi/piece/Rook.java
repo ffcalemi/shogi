e1501e7cbe72412b1298f0054d76d0b18f4ab72f
Ahmad <ahmad.anv@gmail.com>
2016-04-04 19:48:42 +0430

    Implementing calculatingMoves method for Rock class

--- src/shogi/piece/Rook.java
+++ src/shogi/piece/Rook.java
@@ -1,6 +1,7 @@
 package shogi.piece;
 
 
+import shogi.board.GameBoard;
 import shogi.board.Position;
 
 import java.util.ArrayList;
@@ -10,16 +11,45 @@ import java.util.ArrayList;
  * @version 1.0.0
  */
 public class Rook extends ChessMen {
-	public Rook(Position pos, ChessMen.roles role) {
-
-
+	public Rook(Position pos, ChessMen.roles role, GameBoard gameBoard) {
+		super.position = pos;
+		super.setPlayerRole(role);
+		super.gameBoard = gameBoard;
 	}
 
 	@Override
 	public ArrayList<Position> calculatingMoves() {
 		ArrayList<Position> allowedMoves = new ArrayList<>();
+		Position pos;
 
-		return  allowedMoves;
+		//  Move up
+		pos = new Position(this.getPosition().getCol(), this.getPosition().getRow() + 1);
+		while (gameBoard.canGo(this, pos)) {
+			allowedMoves.add(pos);
+			pos = new Position(pos.getCol(), pos.getRow() + 1);
+		}
+
+		//  Move down
+		pos = new Position(this.getPosition().getCol(), this.getPosition().getRow() - 1);
+		while (gameBoard.canGo(this, pos)) {
+			allowedMoves.add(pos);
+			pos = new Position(pos.getCol(), pos.getRow() - 1);
+		}
+
+		//  Move left
+		pos = new Position(this.getPosition().getCol() - 1, this.getPosition().getRow());
+		while (gameBoard.canGo(this, pos)) {
+			allowedMoves.add(pos);
+			pos = new Position(pos.getCol() - 1, pos.getRow());
+		}
+
+		//  Move right
+		pos = new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() + 1);
+		while (gameBoard.canGo(this, pos)) {
+			allowedMoves.add(pos);
+			pos = new Position(pos.getCol() - 1, pos.getRow());
+		}
+		return allowedMoves;
 	}
 
 
