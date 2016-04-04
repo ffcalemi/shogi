f977af22ef6b896006d30286b5fa2b8e69e1b705
Ahmad <ahmad.anv@gmail.com>
2016-04-04 22:18:38 +0430

    Implementing calculatingMoves of the Knight Class

--- src/shogi/piece/Knight.java
+++ src/shogi/piece/Knight.java
@@ -18,8 +18,52 @@ public class Knight extends ChessMen {
 
 	@Override
 	public ArrayList<Position> calculatingMoves() {
+		/**
+		 * First check if it is not updated it can just move forward in L shape or
+		 * if it is updated it can have a move like GoldGeneral
+		 */
 		ArrayList<Position> allowedMoves = new ArrayList<>();
-
+		if (getNormal()) {
+			if (getPlayerRole() == roles.PLAYER_BLACK_ROLE) {
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() + 2)))
+					allowedMoves.add(new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() + 2));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() + 2)))
+					allowedMoves.add(new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() + 2));
+			}else {
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() - 2)))
+					allowedMoves.add(new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() - 2));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() - 2)))
+					allowedMoves.add(new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() - 2));
+			}
+		} else {
+			if (getPlayerRole() == roles.PLAYER_BLACK_ROLE) {
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() - 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() - 1));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() - 1, this.getPosition().getRow())))
+					allowedMoves.add(new Position(this.getPosition().getCol() - 1, this.getPosition().getRow()));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() + 1, this.getPosition().getRow())))
+					allowedMoves.add(new Position(this.getPosition().getCol() + 1, this.getPosition().getRow()));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() + 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() + 1));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol(), this.getPosition().getRow() - 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol(), this.getPosition().getRow() - 1));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol(), this.getPosition().getRow() + 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol(), this.getPosition().getRow() + 1));
+			} else {
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() + 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol() - 1, this.getPosition().getRow() + 1));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() - 1, this.getPosition().getRow())))
+					allowedMoves.add(new Position(this.getPosition().getCol() - 1, this.getPosition().getRow()));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() + 1, this.getPosition().getRow())))
+					allowedMoves.add(new Position(this.getPosition().getCol() + 1, this.getPosition().getRow()));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() - 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol() + 1, this.getPosition().getRow() - 1));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol(), this.getPosition().getRow() + 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol(), this.getPosition().getRow() + 1));
+				if (gameBoard.canGo(this, new Position(this.getPosition().getCol(), this.getPosition().getRow() - 1)))
+					allowedMoves.add(new Position(this.getPosition().getCol(), this.getPosition().getRow() - 1));
+			}
+		}
 		return  allowedMoves;
 	}
 
